399d2c79a23d7f80f87ce7074b82372efb741683
Jenifer Ribeiro <jenifernatali54@gmail.com>
2024-04-24 19:50:19 +0000

    aula 24-04

--- app/src/main/java/application/controller/LivroController.java
+++ app/src/main/java/application/controller/LivroController.java
@@ -1,9 +1,15 @@
 package application.controller;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 import application.model.Livro;
 import application.repository.LivroRepository;
@@ -16,18 +22,43 @@ public class LivroController {
 
     @RequestMapping("/list")
     public String list(Model ui) {
-        Livro[] livros = new Livro[2];
-        livros[0] = new Livro();
-        livros[0].setId(1);
-        livros[0].setTitulo("Livto Teste 1");
-        livros[0].setGenero("G1");
-        livros[1] = new Livro();
-        livros[1].setId(2);
-        livros[1].setTitulo("Livro teste 2");
-        livros[1].setGenero("G2");
 
-        ui.addAttribute("livros", livros);
+        ui.addAttribute("livros", livroRepo.findAll());
 
         return "/livros/list";
     }
+
+    @RequestMapping("/insert")
+        public String insert() {
+            return "/livros/insert";
+    }
+
+    @RequestMapping(value ="/insert", method = RequestMethod.POST)
+    public String insert(
+        @RequestParam("titulo") String titulo, 
+        @RequestParam("genero") String genero){
+            
+            Livro livro = new Livro();
+            livro.setTitulo(titulo);
+            livro.setGenero(genero);
+
+            livroRepo.save(livro);
+
+            return "redirect:/livros/list";
+    }
+
+    @RequestMapping("update/{id}")
+    public String update(Model ui, @PathVariable long id){  /*variavel de caminho pathvariable */
+        Optional<Livro> resultado = livroRepo.findById(id);
+
+      if(resultado.isPresent()){
+        ui.addAttribute("livro", resultado.get());
+        return "/livros/update";
+      }
+
+
+        return "/livros/update";
+    }
+
 }
+
